3abb3b55ce99cbfce003dedab37aae9f709b18c7
mni-dev <nazrulislam01531@gmail.com>
2022-03-25 00:09:04 +0600

    product price issue fixed

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {addToDb, getStoredCart} from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -12,10 +13,16 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, []);
 
+    useEffect(() =>{
+        const storedCart = getStoredCart(); 
+        console.log(storedCart)
+    }, [])
+
     const handleAddToCart= (product)=>{
-        console.log(product);
-        const newCart = [...cart, products];
-        setCart(newCart)
+        // console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+        addToDb(product.id)
     }
     return (
         <div className='shop-container'>
